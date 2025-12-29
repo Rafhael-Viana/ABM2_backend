760b4635fa1eadc57958a469cb2ff2bb12d341f3
Ademar Rafhael <vnarafha@gmail.com>
2025-12-28 23:16:22 -0300

    refactor: melhorar estrutura do modelo Setor
    - Alterar tipo de Quantidade de string para int32
    - Renomear campo By para CreatedBy para clareza

--- models/setor.go
+++ models/setor.go
@@ -6,9 +6,9 @@ type Setor struct {
 	ID         int32      `json:"id"`
 	Setor_ID   string     `json:"user_id"`
 	Nome       string     `json:"name"`
-	Quantidade string     `json:"qtd_users"`
+	Quantidade int32      `json:"qtd_users"`
 	Lider      string     `json:"lider"`
-	By         string     `json:"by"`
+	CreatedBy  string     `json:"createdBy"`
 	CreatedAt  *time.Time `json:"createdAt"`
 	UpdatedAt  *time.Time `json:"updatedAt"`
 }
